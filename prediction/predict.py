9458d6fe15e90848a533df5bc55e2c335852aa80
Kingkaisery <nitisit.j@ku.th>
2018-05-01 01:11:01 -0700

    improve model

--- prediction/predict.py
+++ prediction/predict.py
@@ -29,45 +29,58 @@ def TokenizingTestData(data,start,end,vect,vect2):
 	tests = hstack((des,title))
 	return tests
 
-def getTestData(data,start,end,vect,vect2):
+def getTestData(data,start,end,vect,vect2,vect3,vect4,vect5,vect6,vect7,vect8):
 	targets = []
 	desTests = []
 	titleTests = []
+	levelTests = []
+	orgTests = []
+	loTests = []
+	eduTests = []
+	expTests = []
+	indTests = []	
+	jobfuncTests = []
 	for i in xrange(start,end):
 		job = data[i].data
-		desTests.append(job['level'] + ' ' + job['organization'] + ' ' + job['joblocation'] + ' ' + job['education'] + ' ' + 
-		job['experience'] + ' ' + job['employmentType'] + ' ' + job['industry'] + ' ' + job['jobfunction'])
 		titleTests.append(job['title'])
-	title = vect.transform(titleTests)
-	des = vect2.transform(desTests)
-	tests = hstack((title,des))
-	return tests
-
-def getTestData(data,start,end,vect,vect2):
-	targets = []
-	desTests = []
-	titleTests = []
-	for i in xrange(start,end):
-		job = data[i].data
-		desTests.append(job['level'] + ' ' + job['organization'] + ' ' + job['joblocation'] + ' ' + job['education'] + ' ' + 
-		job['experience'] + ' ' + job['employmentType'] + ' ' + job['industry'] + ' ' + job['jobfunction'])
-		titleTests.append(job['title'])
-	title = vect.transform(titleTests)
-	des = vect2.transform(desTests)
-	tests = hstack((title,des))
+		levelTests.append(job['level'])
+		orgTests.append(job['organization'])
+		loTests.append(job['joblocation'])
+		eduTests.append(job['education'])
+		expTests.append(job['experience'])
+		indTests.append(job['industry'])
+		jobfuncTests.append(job['jobfunction'])
+		#desTests.append(job['level'] + ' ' + job['organization'] + ' ' + job['joblocation'] + ' ' + job['education'] + ' ' + 
+		#job['experience'] + ' ' + job['employmentType'] + ' ' + job['industry'] + ' ' + job['jobfunction'])		
+	title = vect1.transform(titleTests)
+	level = vect2.transform(levelTests)
+	org = vect3.transform(orgTests)
+	lo = vect4.transform(loTests)
+	edu = vect5.transform(eduTests)
+	exp = vect6.transform(expTests)
+	ind = vect7.transform(indTests)
+	jobfunc = vect8.transform(jobfuncTests)
+	#des = vect2.transform(desTests)
+	tests = hstack((title,level,org,lo,edu,exp,ind,jobfunc))
 	return tests
 
 paths = json.load(open('/home/onani/Desktop/SeniorProject_WebApplication/prediction/SETTINGS.json','rb'))
 #print 'Loading models'
 model = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/model.p')
-vect = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect1.p')
+vect1 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect1.p')
 vect2 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect2.p')
+vect3 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect3.p')
+vect4 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect4.p')
+vect5 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect5.p')
+vect6 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect6.p')
+vect7 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect7.p')
+vect8 = joblib.load('/home/onani/Desktop/SeniorProject_WebApplication/prediction/vect8.p')
 
 #print 'loading testing data',paths['TEST_DATA_PATH']
 validData = LoadData('/home/onani/Desktop/SeniorProject_WebApplication/prediction/test.csv')
 titles = [validData[i].data['title'] for i in xrange(len(validData))]
 #validSalary = [validData[i].data['SalaryMid'] for i in xrange(len(validData))]
-validTests = getTestData(validData,0,len(validData),vect,vect2)
+validTests = getTestData(validData,0,len(validData),vect1,vect2,vect3,vect4,vect5,vect6,vect7,vect8)
 
 #print 'predicting'
 predictions = np.exp(model.predict(validTests))
